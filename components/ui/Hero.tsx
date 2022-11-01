2ab38c4c3cc505d3d4193d804301092bfd729d0c
CodeAlb <infuzzione@gmail.com>
2022-11-01 21:59:39 +0100

    Simplify UI

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -8,12 +8,10 @@ interface HeroProps {
 }
 
 const css = {
-  section: 'relative border-b border-black/10 dark:border-white/30',
-  inner: 'py-12 sm:py-14 md:py-16 max-w-5xl mx-auto px-4 text-center',
-  title: 'text-5xl sm:text-6xl md:text-7xl inline-flex font-semibold tracking-tight',
+  section: 'select-none border-b border-black/10 dark:border-white/10',
+  inner: 'h-32 sm:h-40 md:h-48 flex items-center justify-center w-full max-w-5xl mx-auto px-4',
+  title: 'text-4xl sm:text-6xl md:text-7xl font-semibold tracking-tight',
   description: 'sm:text-lg md:text-xl mt-2 sm:mt-4 md:mt-6 max-w-2xl mx-auto tracking-tight',
-  bgGradient: 'absolute inset-0 z-[-1] bg-gradient-to-r',
-  bgShade: 'absolute inset-0 z-[-1] bg-gradient-to-b from-l-bg/80 dark:from-d-bg/80',
 }
 
 export default function Hero({ className, title, description, bgGradient }: HeroProps) {
@@ -23,8 +21,6 @@ export default function Hero({ className, title, description, bgGradient }: Hero
         <h1 className={css.title}>{title}</h1>
         {/* {description && <p className={css.description}>{description}</p>} */}
       </div>
-      <div className={cn(css.bgGradient, bgGradient)} />
-      <div className={css.bgShade} />
     </section>
   )
 }
