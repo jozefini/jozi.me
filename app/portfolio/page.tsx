eb7a362e35bd4be7505ac5f5eaca8d5bc464a5ef
jozefini <jozibashaj@gmail.com>
2023-04-16 19:25:14 +0200

    update pages

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Jozi.me | Portfolio',
+  description: 'Full-stack developer, designer, and writer.',
+}
+
+export default function PortfolioPage() {
+  return <main>Portfolio...</main>
+}
