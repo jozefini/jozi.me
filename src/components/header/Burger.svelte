21872447dc6661f54b65ff191eb958a5d53c3157
jozefini <jozibashaj@gmail.com>
2023-05-24 13:35:59 +0200

    Update

--- src/components/header/Burger.svelte
+++ src/components/header/Burger.svelte
@@ -1,13 +1,34 @@
 <script>
+	import { onMount } from 'svelte'
 	import { offCanvas } from '$lib/stores/offCanvas'
 	import { clickOutside } from '$lib/directives/clickOutside'
+	import { off, on } from '$lib/utils/events'
 	import { cn } from '$lib/utils/helpers'
 	import Magnetic from '../Magnetic.svelte'
 	import OffCanvas from './OffCanvas.svelte'
 
+	let hideBurger = true
+	onMount(() => {
+		const mediaQuery = window.matchMedia('(min-width: 768px)')
+		const handleResize = () => {
+			hideBurger = mediaQuery.matches && window.scrollY < 200
+		}
+		handleResize()
+		on(mediaQuery, 'change', handleResize)
+		on(window, 'scroll', handleResize)
+		return () => {
+			off(mediaQuery, 'change', handleResize)
+			off(window, 'scroll', handleResize)
+		}
+	})
+
 	const { toggle, hide } = offCanvas
 	const css = {
-		magnetic: 'z-50 in-flex centered fixed top-4 right-4 sm:top-5 sm:right-5 md:top-6 md:right-6',
+		wrapper:
+			'z-50 in-flex rounded-full centered fixed top-4 sm:top-5 md:top-6 right-0 transition-transform duration-500',
+		showWrapper: '-translate-x-4 sm:-translate-x-6 md:-translate-x-8',
+		hideWrapper: 'translate-x-20 sm:translate-x-16 md:translate-x-40',
+		magnetic: 'in-flex rounded-full centered',
 		btn: 'in-flex flex-col centered sq-14 sm:sq-16 md:sq-20 rounded-full text-white transition-all',
 		default: 'bg-footer hover:bg-blue-600 gap-2 hover:gap-3',
 		active: 'bg-blue-600 gap-4',
@@ -17,11 +38,13 @@
 </script>
 
 <div use:clickOutside on:outclick={hide}>
-	<Magnetic class={css.magnetic} power={50}>
-		<button class={cn(css.btn, $offCanvas ? css.active : css.default)} on:click={toggle}>
-			<div class={cn(css.line, $offCanvas && css.activeLine)} />
-			<div class={cn(css.line, $offCanvas && css.activeLine)} />
-		</button>
-	</Magnetic>
+	<div class={cn(css.wrapper, !hideBurger || $offCanvas ? css.showWrapper : css.hideWrapper)}>
+		<Magnetic class={css.magnetic} power={50}>
+			<button class={cn(css.btn, $offCanvas ? css.active : css.default)} on:click={toggle}>
+				<div class={cn(css.line, $offCanvas && css.activeLine)} />
+				<div class={cn(css.line, $offCanvas && css.activeLine)} />
+			</button>
+		</Magnetic>
+	</div>
 	<OffCanvas />
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,18 +2,13 @@
 	import '../app.css'
 	import Header from '$components/Header.svelte'
 	import Footer from '$components/Footer.svelte'
-	import { fade } from 'svelte/transition'
-
-	export let data: any
 </script>
 
 <div class="min-h-screen max-w-[100vw] overflow-hidden font-primary fw-400">
 	<Header />
-	{#key data.currentPath}
-		<main class="under-header" in:fade={{ duration: 150 }} out:fade={{ duration: 150, delay: 150 }}>
-			<slot />
-		</main>
-	{/key}
+	<main>
+		<slot />
+	</main>
 	<Footer />
 </div>
 
