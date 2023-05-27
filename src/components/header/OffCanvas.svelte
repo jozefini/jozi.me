1a9fed746cd77c9709e19cfb3c1e331e7402929f
jozefini <jozibashaj@gmail.com>
2023-05-27 16:36:06 +0200

    Add animation

--- src/components/header/OffCanvas.svelte
+++ src/components/header/OffCanvas.svelte
@@ -6,10 +6,13 @@
 	const { hide } = offCanvas
 
 	const css = {
-		wrapper:
-			'fixed max-w-[90%] w-[32rem] transition-all duration-500 h-full top-0 right-0 z-40 bg-footer',
-		shown: 'translate-x-0',
-		hidden: 'translate-x-full',
+		wrapper: 'fixed max-w-[90%] w-[32rem] duration-[600ms] h-full top-0 right-0 z-40',
+		bgColor: 'absolute right-0 top-0 w-full h-full',
+		showWrapper: 'translate-x-0',
+		hideWrapper: 'translate-x-full',
+		edge: 'w-full absolute left-0 h-full bg-footer duration-[600ms]',
+		showEdge: 'rounded-l-0 delay-200',
+		hideEdge: 'rounded-l-[50%] delay-75',
 		overlay: 'fixed inset-0 bg-black/30'
 	}
 </script>
@@ -17,11 +20,15 @@
 {#if $offCanvas}
 	<div
 		class={css.overlay}
-		in:fade={{ duration: 300 }}
-		out:fade={{ duration: 200 }}
+		in:fade={{ duration: 500 }}
+		out:fade={{ duration: 300 }}
 		on:click={hide}
 		on:keyup={hide}
 	/>
 {/if}
 
-<div class={cn(css.wrapper, $offCanvas ? css.shown : css.hidden)} />
+<div class={cn(css.wrapper, $offCanvas ? css.showWrapper : css.hideWrapper)}>
+	<div class={cn(css.edge, $offCanvas ? css.showEdge : css.hideEdge)} />
+	<div class={css.bgColor} />
+	<div class={cn(css.inner, $offCanvas ? css.showInner : css.hideInner)} />
+</div>
