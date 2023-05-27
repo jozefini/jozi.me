f82a70c6650bbcaf5f18ae524a63e48d4c9f959a
jozefini <jozibashaj@gmail.com>
2023-05-27 16:36:20 +0200

    Remove unused code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,22 +6,10 @@
 
 <div class="min-h-screen max-w-[100vw] overflow-hidden font-primary fw-400">
 	<Header />
+
 	<main>
 		<slot />
 	</main>
+
 	<Footer />
 </div>
-
-<!-- 
-<style>
-	main {
-		display: grid;
-		grid-template-rows: 1fr;
-		grid-template-columns: 1fr;
-	}
-	main > :global(*) {
-		grid-row: 1;
-		grid-column: 1;
-	}
-</style>
- -->
